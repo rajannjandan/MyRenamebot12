7e44d860b3827685b7d84eb5e16969ec74f6a7ba
Powerap2tech <120239369+Powerap2tech@users.noreply.github.com>
2023-03-14 22:00:19 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,13 +12,13 @@ logging.basicConfig(
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", "11329245"))
-    API_HASH = os.environ.get("API_HASH", "90988dcdd5ddd4d0a28843a1e2605924")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5716894802:AAFM4A-oPCuy1FjruRfgZK04bbo7W1pcB-Y")
+    API_ID = int(os.environ.get("API_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
     LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", 1364321375))
+    OWNER_ID = int(os.environ.get("OWNER_ID", ))
     PRO_USERS = OWNER_ID
-    MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://AmRenamebot:AmRenamebot@cluster0.qjhucea.mongodb.net/?retryWrites=true&w=majority")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001583149598"))
+    MONGODB_URI = os.environ.get("MONGODB_URI", "")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
