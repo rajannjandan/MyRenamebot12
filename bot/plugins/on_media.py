ce91799743c8ae43061b9ba2e40fcc1c2aa4c380
rajannjandan <121632208+rajannjandan@users.noreply.github.com>
2023-04-14 20:49:04 +0530

    Update on_media.py

--- bot/plugins/on_media.py
+++ bot/plugins/on_media.py
@@ -16,7 +16,6 @@ async def on_media_handler(c: Client, m: "types.Message"):
     if not m.from_user:
         return await m.reply_text("I don't know about you sar :(")
     await add_user_to_database(c, m)
-    await asyncio.sleep(0)
     await c.send_flooded_message(
         chat_id=m.chat.id,
         text="**Should I show File Information?**",
