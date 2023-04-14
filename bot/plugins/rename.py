762892fde0f3b8abf52fe666bd8adafa8b07e880
rajannjandan <121632208+rajannjandan@users.noreply.github.com>
2023-04-14 20:58:21 +0530

    Update rename.py

--- bot/plugins/rename.py
+++ bot/plugins/rename.py
@@ -26,7 +26,7 @@ from bot.core.handlers.big_rename import handle_big_rename
 
 
 @Client.on_callback_query(filters.regex('rename'))
-async def rename_handlers(bot,update):
+async def rename_handlers(client, message):
     # Checks
     if not m.from_user:
         return await m.reply_text("I don't know about you sar :(")
