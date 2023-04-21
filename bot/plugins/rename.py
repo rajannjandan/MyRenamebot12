7e5d3ac5a8cb20468e039c34fc4d1d33738574cb
rajannjandan <121632208+rajannjandan@users.noreply.github.com>
2023-04-21 15:15:30 +0530

    Update rename.py

--- bot/plugins/rename.py
+++ bot/plugins/rename.py
@@ -28,7 +28,7 @@ from bot.core.handlers.big_rename import handle_big_rename
 @Client.on_callback_query(filters.regex(['rename']) & filters.private & ~filters.edited)
 async def rename_handler(c: Client, m: Message):
 
-    if m.data == "rename_file":
+    if not m.from_user:
         return await m.reply_text("I don't know about you sar :(")
         is_in_gap, sleep_time = await check_time_gap(m.from_user.id)
         if is_in_gap:
